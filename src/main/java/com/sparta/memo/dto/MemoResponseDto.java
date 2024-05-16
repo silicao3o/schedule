513d35b1b74e332d9b07fc9739aa54018445972b
silicao3o <zergskybmw@gmail.com>
2024-05-16 21:06:37 +0900

    JDBC

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -4,14 +4,20 @@ import com.sparta.memo.entity.Memo;
 import lombok.Getter;
 
 @Getter
-public class MeomoResponseDto {
+public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
 
-    public MeomoResponseDto(Memo memo) {
+    public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
+    }
+
+    public MemoResponseDto(Long id, String username, String contents) {
+        this.id = id;
+        this.username = username;
+        this.contents = contents;
     }
 }
